7436e184102ecf5b65bc8b1a2a95bab24e38157f
karinbellander <karinbellander@gmail.com>
2017-11-23 13:46:26 +0100

    Updated design

--- code/script.js
+++ code/script.js
@@ -1,10 +1,7 @@
-
-
-
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=d3db423c5d668725b71a3527c28df71e").then((response) => {
   return response.json()
 }).then((json) => {
   document.getElementById("city").innerHTML = (json.name)
   document.getElementById("temperature").innerHTML = (json.main.temp)
-  document.getElementById("description").innerHTML = (json.weather[1])
+  document.getElementById("description").innerHTML = (json.weather[0].description)
 })
